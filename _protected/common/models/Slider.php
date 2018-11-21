16197d40d7cb0a86c3ee1d9a7bf0f494ab283f64
sandeepgwebs <sandeep.gwebs@gmail.com>
2018-11-21 17:02:09 +0530

    upload project asdf

---
+++ _protected/common/models/Slider.php
@@ -0,0 +1,44 @@
+<?php
+
+namespace common\models;
+
+use Yii;
+
+/**
+ * This is the model class for table "slider".
+ *
+ * @property int $id
+ * @property string $name
+ */
+class Slider extends \yii\db\ActiveRecord
+{
+    /**
+     * {@inheritdoc}
+     */
+    public static function tableName()
+    {
+        return 'slider';
+    }
+
+    /**
+     * {@inheritdoc}
+     */
+    public function rules()
+    {
+        return [
+            [['name'], 'string', 'max' => 255,],
+            [['name'], 'required'],
+        ];
+    }
+
+    /**
+     * {@inheritdoc}
+     */
+    public function attributeLabels()
+    {
+        return [
+            'id' => 'ID',
+            'name' => 'Name',
+        ];
+    }
+}
